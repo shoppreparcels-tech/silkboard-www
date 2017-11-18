80bd233227ce3975a9b6635f7630bc0830c41f0c
vikasjson <v9012367521>
2017-11-18 17:17:54 +0530

    Fix : package delition complete

--- app/Package.php
+++ app/Package.php
@@ -3,12 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Package extends Model
 {
     use SoftDeletes;
-    protected $softDelete = true;
 
     protected $dates = ['deleted_at'];
 
