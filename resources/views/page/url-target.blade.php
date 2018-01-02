8523cf3c6db137e6b23a5761d5e9613a3dbee0b8
Meena <meena@shoppre.com>
2018-01-02 17:55:38 +0530

    fix:meta tag added to url page

--- resources/views/page/url-target.blade.php
+++ resources/views/page/url-target.blade.php
@@ -1,15 +1,20 @@
 @extends('layout')
 
 @section('title',  'Shipping From India To '.ucfirst(trans($country)).' - Courier Services To '.ucfirst(trans($country)))
-@section('description', 'Shoppre offers Door to Door courier service to ' .ucfirst(trans($country)). ' from any part of India. The charges are cheapest in India for sending courier to' .ucfirst(trans($country)).'. Sign Up Now!')
-@section('keywords', 'ship your packages, delivered to your country, parcel services to '.ucfirst(trans($country)).', sending courier to ' .ucfirst(trans($country)).', shipping')
-
+@section('description', 'Shoppre offers Door to Door courier service to ' .ucfirst(trans($country)).
+' from any part of India. The charges are cheapest in India for sending courier to' .ucfirst(trans($country)).'. Sign Up Now!')
+@section('keywords', 'ship your packages, delivered to your country, parcel services to '.ucfirst(trans($country)).',
+sending courier to ' .ucfirst(trans($country)).', shipping')
+<meta name="robots" content="noydir, noodp"/>
 @section('content')
   <section class="timeline">
       <div class="container">
           <h1>Shipping from India to {{ucfirst(trans($country))}}</h1>
           <br>
-          <h4>Ship from India to {{ucfirst(trans($country))}}. Shoppre offers DOOR TO DOOR courier to {{ucfirst(trans($country))}} from ANY part of India. The charges are cheapest in India for sending courier to {{ucfirst(trans($country))}} . This is by a courier company which is the best in industry for parcel services to {{ucfirst(trans($country))}} .</h4>
+          <h4>Ship from India to {{ucfirst(trans($country))}}. Shoppre offers DOOR TO DOOR courier to
+              {{ucfirst(trans($country))}} from ANY part of India. The charges are cheapest in India for sending
+              courier to {{ucfirst(trans($country))}} . This is by a courier company which is the best in industry
+              for parcel services to {{ucfirst(trans($country))}} .</h4>
       </div>
   </section>
 @endsection
