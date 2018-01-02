3d020f5ca8be6837ea254566dedbef9a3d889b1c
Meena <meena@shoppre.com>
2018-01-02 19:49:41 +0530

    fix:sitemap xml updated with 9000 urls

--- resources/views/page/url-target.blade.php
+++ resources/views/page/url-target.blade.php
@@ -2,7 +2,7 @@
 
 @section('title',  'Shipping From India To '.ucfirst(trans($destination)).' - Courier Services To '.ucfirst(trans($destination)))
 @section('description', 'Shoppre offers Door to Door courier service to ' .ucfirst(trans($destination)).
-' from any part of India. The charges are cheapest in India for sending courier to' .ucfirst(trans($destination)).'. Sign Up Now!')
+' from any part of India. The charges are cheapest in India for sending courier to ' .ucfirst(trans($destination)).'. Sign Up Now!')
 @section('keywords', 'ship your packages, delivered to your country, parcel services to '.ucfirst(trans($destination)).',
 sending courier to ' .ucfirst(trans($destination)).', shipping')
 
