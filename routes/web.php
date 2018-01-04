ae536e30ede50817b4e0f6dbd4d02a3c54f0ba40
Meena <meena@shoppre.com>
2018-01-04 17:33:43 +0530

    fix:sitemap xml updated with 28000 urls

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/reviews', 'PageController@reviews')->name('reviews');
 Route::post('/reviews/submit', 'PageController@submitReview')->name('reviews.submit');
 Route::get('/{initial}-from-{source}-to-{destination}', 'PageController@urlTargetShipping')->name('urlTargetShipping');
 Route::get('/{initial}-{content}-from-{source}-to-{destination}', 'PageController@urlTargetSend')->name('urlTargetSend');
+Route::get('/{initial}-{cprefix}-{cpostfix}-from-{source}-to-{destination}', 'PageController@urlTargetContent')->name('urlTargetContent');
 Route::get('/schedule-pickup', 'SchedulePickupController@index')->name('schedulePickup.Index');
 Route::post('/schedule-pickup', 'SchedulePickupController@submit')->name('schedulePickup.Submit');
 Route::get('/schedule-pickup/confirm', 'SchedulePickupController@confirm')->name('schedulePickup.confirmPickup');
