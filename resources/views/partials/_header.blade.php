6e6467c5224ac3f2e2eef571a031198739113bfa
Manjesh V <manjesh@quetzal.in>
2018-03-04 17:37:13 +0530

    Update _header.blade.php

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -17,6 +17,8 @@
                     <li><a href="{{route('partner')}}">Become a Partner</a></li>
                     <li><a href="{{route('faq')}}">FAQ</a></li>
                     <li><a href="http://blog.shoppre.com/" target="_blank">Blogs</a></li>
+                    <li><a href="https://www.quora.com/topic/Shoppre-com" target="_blank">Ask Questins</a></li>
+
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li class="padding_li1" href="mailto:support@shoppre.com"><i class="fa fa-envelope icon_margen"></i>support@shoppre.com
