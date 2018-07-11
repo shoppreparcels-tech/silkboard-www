9e969e70b52cbcf855357ae1fdb75941fccce165
Vikas Kumar <vikasjson@gmail.com>
2018-07-11 21:44:29 +0530

    fix: zendesk wedget changed (#211)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -375,7 +375,7 @@
 {{--</script>--}}
 <!--End of Tawk.to Script-->
 
-<!-- Start of shoppre-international-shipping-courier Zendesk Widget script -->
+<!--zendesk-chat Start of shoppre-international-shipping-courier Zendesk Widget script -->
 <script>/*<![CDATA[*/window.zE||(function(e,t,s){var n=window.zE=window.zEmbed=function(){n._.push(arguments)}, a=n.s=e.createElement(t),r=e.getElementsByTagName(t)[0];n.set=function(e){ n.set._.push(e)},n._=[],n.set._=[],a.async=true,a.setAttribute("charset","utf-8"), a.src="https://static.zdassets.com/ekr/asset_composer.js?key="+s, n.t=+new Date,a.type="text/javascript",r.parentNode.insertBefore(a,r)})(document,"script","2ab4b736-3ac8-4f95-8b6f-e2c9d6325182");/*]]>*/</script>
 <!-- End of shoppre-international-shipping-courier Zendesk Widget script -->
 
