85d9f3071a04697d2170a3fa85b0c88151edce99
Shoppre <tech.shoppre@gmail.com>
2019-05-01 11:33:53 +0530

    feat(Branch): branch revenue tracking integration (#872) (#873)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -77,6 +77,15 @@
     </script>
 <!-- End one signal -->
 
+<!-- Start branch -->
+    <script>
+        // load Branch
+        (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode autoAppIndex banner closeBanner closeJourney creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode trackCommerceEvent logEvent disableTracking".split(" "), 0);
+        // init Branch
+        branch.init('key_live_dpUiXdx8wJuDUbRMgEpMBefpuBcQCx32');
+    </script>
+<!-- End Branch -->
+
     <!-- Google Tag Manager -->
     <script>(function (w, d, s, l, i) {
             w[l] = w[l] || [];
