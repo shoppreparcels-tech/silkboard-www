63d7a3750b0c6ee0a3550b420047f8ceeaee0c02
Aloak Jawali <31075700+aloak@users.noreply.github.com>
2019-12-05 16:51:55 +0530

    fix(Verification Email): Verification Email and welcome email fixed (#1411) (#1412)

--- resources/views/myaccount/email/verification.blade.php
+++ resources/views/myaccount/email/verification.blade.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
+<html xmlns=http://www.w3.org/1999/xhtml xmlns:v=urn:schemas-microsoft-com:vml xmlns:o=urn:schemas-microsoft-com:office:office>
+<head>
+   <title>ShoppRe.com - Verification Email</title>
+   <meta content="text/html; charset=UTF-8" http-equiv=Content-Type>
+   <meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1">
+</head>
+<body>
 <table width="100%">
    <tbody>
-      <tr>
-         <td valign="middle">
+   <tr>
+      <td valign="middle">
          <p>Hi <span><a target="_blank" href="mailto:{{$email}}">{{$name}}</a>!</span></p>
          <p>Greetings from Shoppre!</p>
          <br>
@@ -13,11 +21,11 @@
          <p>Please verify Your Email Address by clicking the button below.</p>
          <table width="335">
             <tbody>
-               <tr>
-                  <td align="center" valign="middle" bgcolor="#00c9c9" style="word-break:break-word;border-collapse:collapse!important;border-radius:35px;padding:20px 25px">
-                     <a target="_blank" href="{{$link}}" style="color:#fff!important;text-decoration:none;display:block;font-size:23px;font-style:italic">Confirm my account</a>
-                  </td>
-               </tr>
+            <tr>
+               <td align="center" valign="middle" bgcolor="#00c9c9" style="word-break:break-word;border-collapse:collapse!important;border-radius:35px;padding:20px 25px">
+                  <a target="_blank" href="{{$link}}" style="color:#fff!important;text-decoration:none;display:block;font-size:23px;font-style:italic">Confirm my account</a>
+               </td>
+            </tr>
             </tbody>
          </table>
          <br>
@@ -28,13 +36,13 @@
             <br><br>
             Note:This is a system alert message. Please do not reply to this email
             <br>
-            <p>
+         <p>
             <a href="tel:+91 80 4049 4077">+91 80 4049 4077</a> | <a href="mailto:support@shoppre.com">support@shoppre.com</a> | <a href="https://www.google.co.in/maps/place/Shoppre.com+-+Your+Global+Shipping+Partner/@12.925805,77.6332563,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae146191401f95:0x1cf2770c1532d7d1!8m2!3d12.9257998!4d77.635445"> #181, 1st Floor, 2nd Cross Rd, 1st Block Koramangala, Bengaluru, Karnataka 560034</a>
-
-
-            </p>
          </p>
-         </td>
-      </tr>
+         </p>
+      </td>
+   </tr>
    </tbody>
 </table>
+</body>
+</html>
