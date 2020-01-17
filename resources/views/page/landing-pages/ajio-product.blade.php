502e9b1b71c917c2c6cbf4c5546b8ab37fcb8b2a
Vikas Kumar <vikasjson@gmail.com>
2020-01-17 11:52:54 +0530

    Staging (#1491)
    * fix(Ajio landing): Content update in ajio landing page. (#1490)

--- resources/views/page/landing-pages/ajio-product.blade.php
+++ resources/views/page/landing-pages/ajio-product.blade.php
@@ -115,7 +115,7 @@
         <div class="container fst-service" style="margin-top: 0px;">
             <div class="row">
                 <div class="">
-                    <h2 class="f-s-18 p-color-cement-dark font-weight-900 txt-center"><span class="f-s-25 f-c-red f-w-9">MAKE IT A STYLISH (40-80% OFF)</span> /
+                    <h2 class="f-s-18 p-color-cement-dark font-weight-900 txt-center"><span class="f-s-25 f-c-red f-w-9">EPIC BLOWOUT (FLAT 55% OFF)</span> /
                         Below are the Recommended & Recently Shipped Products!</h2>
 
                 </div>
