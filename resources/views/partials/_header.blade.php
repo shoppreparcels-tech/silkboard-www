6ebfcd1d41c7d9e4c3dd510ec90d8a9da168e02c
Bibhas Ghoshal <bibhasghoshal44.bg@gmail.com>
2020-05-18 12:44:25 +0530

    Fix(UI): UI update. (#1661) (#1662)
    Co-authored-by: Bibhas Ghoshal <bibhas@shoppre.com>

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -201,7 +201,7 @@
                     </li>
                     <li><a href="{{route('offers-new.index')}}" class="shoppre-offers">Offers </a></li>
                     <li><a href="{{route('pricing')}}">Shipping Rates</a></li>
-                    <li><a href="https://www.shoppreparcels.com/covid19-note/" target="_blank">
+                    <li><a href="https://www.shoppreparcels.com/service-alerts/" target="_blank">
                             <span style="color: #f9c030;" id="offer">COVID19</span>
 {{--                            <sup class="p-color-red" style="font-size: 8px" id="offer">Buy</sup>--}}
                         </a>
